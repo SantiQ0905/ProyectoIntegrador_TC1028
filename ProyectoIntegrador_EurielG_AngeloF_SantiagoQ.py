00e484881e3f090fed664e263080924cd6d6cae6
Santiago Quintana <59663686+SantiQ0905@users.noreply.github.com>
2023-10-10 17:01:33 -0600

    Header, and first details.

--- ProyectoIntegrador_EurielG_AngeloF_SantiagoQ.py
+++ ProyectoIntegrador_EurielG_AngeloF_SantiagoQ.py
@@ -1,1 +1,22 @@
-print("Hello World!")
+#   ___  ___  ______  _____________________    _____  ____________________  ___   ___  ____  ___ 
+#  / _ \/ _ \/ __ \ \/ / __/ ___/_  __/ __ \  /  _/ |/ /_  __/ __/ ___/ _ \/ _ | / _ \/ __ \/ _ \
+# / ___/ , _/ /_/ /\  / _// /__  / / / /_/ / _/ //    / / / / _// (_ / , _/ __ |/ // / /_/ / , _/
+#/_/  /_/|_|\____/ /_/___/\___/ /_/  \____/ /___/_/|_/ /_/ /___/\___/_/|_/_/ |_/____/\____/_/|_| 
+#                      
+# SISTEMA DE INVENTARIO: TIENDA DEPARTAMENTAL DE ELECTRONICOS
+#                                                                          
+# Pensamiento Computacional para la ingeniería
+# Sergio Adán Flores Cantú
+#
+# --- EQUIPO 5 ---
+# Santiago Quintana Moreno A01571222                                                                                      
+# Euriel Gomez Tamez A00838859
+# Angelo Franco Baez A00840411                                                                                          
+
+import time
+
+print("Bienvenido al Sistema Interno de Inventario, Control, Venta, Registro y Seguimiento.")
+time.sleep(1)
+
+print("Hello World!!")
+
