0e0b6c405b06976c2b91bacef1dc930e74b6fe91
Santiago Quintana <59663686+SantiQ0905@users.noreply.github.com>
2023-10-11 12:45:08 -0600

    Pasword Initiation

--- ProyectoIntegrador_EurielG_AngeloF_SantiagoQ.py
+++ ProyectoIntegrador_EurielG_AngeloF_SantiagoQ.py
@@ -11,12 +11,71 @@
 # --- EQUIPO 5 ---
 # Santiago Quintana Moreno A01571222                                                                                      
 # Euriel Gomez Tamez       A00838859
-# Angelo Franco Baez       A00840411                                                                                          
+# Angelo Franco Baez       A00840411     
+
 
 import time
 
-print("Bienvenido al Sistema Interno de Inventario, Control, Venta, Registro y Seguimiento.")
+logo = """
+ _       _____    ____  ___________
+| |     / /   |  / __ \/ ____/ ___/
+| | /| / / /| | / /_/ / __/  \__ \ 
+| |/ |/ / ___ |/ _, _/ /___ ___/ / 
+|__/|__/_/  |_/_/ |_/_____//____/  
+                                   
+"""
+
+print(logo)
+print("Warehouse Arrival and Records, Inventory Evaluation, Sales Data, Sales Reports System")
+
 time.sleep(1)
 
-print("Hello World!!")
+usuariosPermitidos = ["admin", "EurielGT", "AngeloFB", "SantiagoQM", "SergioF"]
+pinesPermitidos = ["admin", "EGT260106", "AFB291103", "SQM090503", "TC1028"]
+intentosMaximos = 3
+    
 
+def verificacionContrasenia(pinesPermitidos):
+    for intento in range(1, intentosMaximos + 1):
+        pinIngresado = input(f"Intento {intento}: Ingrese el PIN: ")
+
+        if pinIngresado == pinesPermitidos:
+            print("Login Correcto")
+            break 
+        else:
+            print("PIN incorrecto")
+
+    else:
+        print("Llamando a la policía")
+
+"""
+def verificar_usuario_contraseña():
+    usuarios = {
+        "usuario1": "contraseña1",
+        "usuario2": "contraseña2",
+        "usuario3": "contraseña3"
+    }
+    usuario = input("Ingrese su nombre de usuario: ")
+    contraseña = input("Ingrese su contraseña: ")
+    
+    for user, password in usuarios.items():
+        if usuario == user and contraseña == password:
+            print("Acceso concedido.")
+            break
+    else:
+        print("Usuario o contraseña incorrectos.")
+
+verificar_usuario_contraseña()
+"""
+
+
+
+
+print("---- MENU ---- \n")
+
+print("[1] = Cuenta números.")
+print("[2] = Suma de N números consecutivos.")
+print("[3] = Lista de precios.")
+print("[4] = Promedio Sencillo.")
+print("[5] = Promedio con Desición.")
+1
