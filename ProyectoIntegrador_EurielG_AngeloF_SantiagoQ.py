7bb4948d444c902c311822dc7881398d76cf08a1
Santiago Quintana <59663686+SantiQ0905@users.noreply.github.com>
2023-10-10 18:38:03 -0600

    updates

--- ProyectoIntegrador_EurielG_AngeloF_SantiagoQ.py
+++ ProyectoIntegrador_EurielG_AngeloF_SantiagoQ.py
@@ -10,8 +10,8 @@
 #
 # --- EQUIPO 5 ---
 # Santiago Quintana Moreno A01571222                                                                                      
-# Euriel Gomez Tamez A00838859
-# Angelo Franco Baez A00840411                                                                                          
+# Euriel Gomez Tamez       A00838859
+# Angelo Franco Baez       A00840411                                                                                          
 
 import time
 
